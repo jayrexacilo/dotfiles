712df0a6617b0e699ec4044193afd4a790d590fb
jayrex <acilojayrex@gmail.com>
2021-01-07 18:57:00 +0800

    cleaning

--- symlinks.sh
+++
@@ -1,7 +0,0 @@
-ln -s ./.config ~/.config
-ln -s ./.themes ~/.themes
-ln -s ./.vim ~/.vim
-ln -s ./.vimrc ~./.vimrc
-ln -s ./.xinitrc ~/.xinitrc
-ln -s ./bash_profile ~/.bash_profile
-ln -s ./bashrc ~/.bashrc
