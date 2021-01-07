4ac78e3b28967cdcdb42b3cd1f28bd6d2cb3914d
jayrex <acilojayrex@gmail.com>
2021-01-07 14:36:16 +0800

    package

--- install_packages.sh
+++ install_packages.sh
@@ -1,1 +1,1 @@
-pacman -S pulseaudio pulseaudio-alsa xorg xorg-server i3-wm dmenu rofi sudo i3status alacritty xterm lxappearance base-devel
+pacman -S pulseaudio pulseaudio-alsa xorg xorg-server xorg-xinit i3-wm dmenu rofi sudo i3status alacritty xterm lxappearance base-devel
